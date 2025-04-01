1d9ef4ad5bd4d8698d3e16f3153184170a4dda1f
Salve <salvek.mtb@gmail.com>
2025-04-01 09:35:18 +0200

    reszta zadan

--- cw4/zad4.py
+++ cw4/zad4.py
@@ -4,3 +4,11 @@
 # b. Porównaj wynik z przesunięciem wykonanym wcześniej przez cv2.warpAffine .
 # Czy zauważyłeś różnice?
 
+import cv2
+import imutils
+
+image = cv2.imread("C:/Users/Salve/OneDrive/Obrazy/MYSZU.jpeg")
+shifted = imutils.translate(image, 50, 100)
+cv2.imshow("Shifted MYSZU", shifted)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- cw4/zad5.py
+++ cw4/zad5.py
@@ -3,3 +3,20 @@
 # poprzez wprowadzenie ich z klawiatury (np. przy użyciu input())
 # b. Sprawdź, jak działa przesunięcie dla różnych wartości.
 
+import sys
+import cv2
+import imutils
+
+shift_params = {"x": 0, "y": 0}
+for key in shift_params.keys():
+    try:
+        shift_params[key] = int(input(f"Enter {key.upper()} shift parameter (int:\n"))
+    except Exception:
+        print(f"Invalid {key.upper()} parameter entered.\n")
+        sys.exit()
+
+image = cv2.imread("C:/Users/Salve/OneDrive/Obrazy/MYSZU.jpeg")
+shifted = imutils.translate(image, shift_params["x"], shift_params["y"])
+cv2.imshow("Shifted MYSZU", shifted)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
